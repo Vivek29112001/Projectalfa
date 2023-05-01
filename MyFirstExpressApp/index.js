dee9bd07b02e1da6273a76fb3ea1c20dc0712b1e
vivek <vivek15292001@gmail.com>
2023-05-01 19:19:05 +0530

    day2

--- MyFirstExpressApp/index.js
+++ MyFirstExpressApp/index.js
@@ -6,16 +6,48 @@ const app = express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+
+var contactList = [
+    {
+        name:"Babita",
+        phone:"9837193333"
+    },
+    {
+        name:"Tony Stack",
+        phone:"1234567890"
+    },
+    {
+        name:"Vivek",
+        phone:"7535838290"
+    }
+
+]
 
 
 app.get('/', function(req,res){
 
-    return res.render('home',{title:"I am Flying"});
+    return res.render('home',{
+        title:"Contacts List",
+        contact_list: contactList
+    });
 });
+
 app.get('/practice', function(req,res){
     return res.render('practice',{
         title:"Let us Play with ejs"
     });
+});
+
+
+app.post('/create-contact', function(req,res){
+//    contactList.push({
+//     name:req.body.name,
+//     phone:req.body.phone
+//    });
+contactList.push(req.body);
+
+return res.redirect('back');
 });
 
 
